6f664cc45f1e2d116700224814d6661fde7159fa
Brandon Barnes <Imbrandonbarnes@gmail.com>
2023-10-05 21:38:41 -0400

    Setup Query to Weather API

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,61 @@
-alert("Hello World!");
-
 $(document).ready(function () {
   // Test jQuery
-  $("h1").text("jQuery is working!");
+  PopAlert("Document Loaded Successfully!", "success", 2000);
+  $("#Input-Button").on("click", SearchCity);
 });
+let geoData;
+
+function SearchCity(event) {
+  event.preventDefault();
+
+  let key = `5ceff8d977d5d19c7bead1274202f97a`;
+  let city = $("#Input-City").val();
+  let url_Geo = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${key}`;
+  let ourOptions = {
+    method: "GET",
+    cache: "reload",
+  };
+
+  getApiJson(url_Geo, ourOptions).then((jsonData) => {
+    if (jsonData) {
+      if (jsonData.length == 0) {
+        console.log("Unknown City");
+      } else {
+        console.log(jsonData); // Print the JSON data to the console
+        let geoData = jsonData[0];
+        let lat = jsonData[0].lat;
+        let lon = jsonData[0].lon;
+        let url_weather = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`;
+
+        getApiJson(url_weather, ourOptions).then((jsonData) => {
+          if (jsonData) {
+            if (jsonData.length == 0) {
+              console.log("Unknown City");
+            } else {
+              console.log(jsonData); // Print the JSON data to the console
+              PopAlert(`Loaded ${city} Successfully!`, "success", 2000);
+              LoadPage(jsonData, geoData);
+            }
+          }
+        });
+      }
+    }
+  });
+}
+
+function LoadPage(data, geo) {
+  console.log(data);
+  $(`#City-Name`).text(`${geo.name}, ${StateToAbbr(geo.state)}`);
+  $(`#Current-Temp`).html(`${Math.round(data.current.temp)}`);
+  let date = UnixToDate(data.current.dt);
+  console.log(date.toDateString);
+  console.log(
+    `${date.getHours().toString()}:${date
+      .getMinutes()
+      .toString()
+      .padStart(2, "0")}`
+  );
+  $(`#Percipitation`).text(`Precipitation`);
+  $(`#Humidity`).text(`Precipitation`);
+  $(`#Wind`).text(`Precipitation`);
+}
---
+++ assets/scripts/utilities.js
@@ -0,0 +1,129 @@
+let alertTimer = null;
+
+/**
+ * Displays an alert message with specified text, type, and duration.
+ *
+ * @param {string} text - The text message to display in the alert.
+ * @param {string} type - The type of alert (e.g., "success" or "info").
+ * @param {number} duration - The duration in milliseconds for the alert to remain visible.
+ */
+function PopAlert(text, type, duration) {
+  let alert = $("#Alert");
+  alert.removeClass("alert-info");
+  alert.removeClass("alert-success");
+
+  switch (type) {
+    case "success":
+      alert.addClass("alert-success");
+      break;
+    case "info":
+      alert.addClass("alert-info");
+      break;
+  }
+
+  alert.attr(
+    "style",
+    "position: absolute; top: 0; width: 100%; visibility: visible;"
+  );
+  alert.text(text);
+
+  clearInterval(alertTimer);
+
+  alertTimer = setInterval(function () {
+    $("#Alert").attr(
+      "style",
+      "position: absolute; top: 0; width: 100%; visibility: hidden;"
+    );
+    clearInterval(alertTimer);
+  }, duration);
+}
+
+/**
+ * Fetches data from a specified API endpoint with customizable options and returns it as a JSON object.
+ *
+ * @param {string} requestUrl - The URL of the API endpoint to fetch data from.
+ * @param {Object} options - Customizable options for the fetch request (e.g., headers, method).
+ * @returns {Promise<any>} A Promise that resolves with the fetched JSON data or rejects with an error message.
+ */
+async function getApiJson(requestUrl, options) {
+  try {
+    const response = await fetch(requestUrl, options);
+
+    if (!response.ok) {
+      throw new Error(
+        `Error Getting API: ${response.status + response.statusText}`
+      );
+    }
+
+    console.log(`Successfully fetched api at ${requestUrl}`);
+    return await response.json();
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+function StateToAbbr(state) {
+  for (let element of states) {
+    if (state === element[0]) {
+      return element[1];
+    }
+  }
+  return "NA";
+}
+
+let states = [
+  ["Arizona", "AZ"],
+  ["Alabama", "AL"],
+  ["Alaska", "AK"],
+  ["Arkansas", "AR"],
+  ["California", "CA"],
+  ["Colorado", "CO"],
+  ["Connecticut", "CT"],
+  ["Delaware", "DE"],
+  ["Florida", "FL"],
+  ["Georgia", "GA"],
+  ["Hawaii", "HI"],
+  ["Idaho", "ID"],
+  ["Illinois", "IL"],
+  ["Indiana", "IN"],
+  ["Iowa", "IA"],
+  ["Kansas", "KS"],
+  ["Kentucky", "KY"],
+  ["Louisiana", "LA"],
+  ["Maine", "ME"],
+  ["Maryland", "MD"],
+  ["Massachusetts", "MA"],
+  ["Michigan", "MI"],
+  ["Minnesota", "MN"],
+  ["Mississippi", "MS"],
+  ["Missouri", "MO"],
+  ["Montana", "MT"],
+  ["Nebraska", "NE"],
+  ["Nevada", "NV"],
+  ["New Hampshire", "NH"],
+  ["New Jersey", "NJ"],
+  ["New Mexico", "NM"],
+  ["New York", "NY"],
+  ["North Carolina", "NC"],
+  ["North Dakota", "ND"],
+  ["Ohio", "OH"],
+  ["Oklahoma", "OK"],
+  ["Oregon", "OR"],
+  ["Pennsylvania", "PA"],
+  ["Rhode Island", "RI"],
+  ["South Carolina", "SC"],
+  ["South Dakota", "SD"],
+  ["Tennessee", "TN"],
+  ["Texas", "TX"],
+  ["Utah", "UT"],
+  ["Vermont", "VT"],
+  ["Virginia", "VA"],
+  ["Washington", "WA"],
+  ["West Virginia", "WV"],
+  ["Wisconsin", "WI"],
+  ["Wyoming", "WY"],
+];
+
+function UnixToDate(unix) {
+  return (date = new Date(unix * 1000));
+}
